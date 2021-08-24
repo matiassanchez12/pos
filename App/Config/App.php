93c58b7fb11b67b6e579859ca619534ad29380a8
Matias Sanchez <52495199+matiassanchez12@users.noreply.github.com>
2021-08-23 22:56:56 -0300

    Update App.php

--- App/Config/App.php
+++ App/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/pos/public/';
+	public $baseURL = 'https://pos-app-s.herokuapp.com/public/';
 
 	/**
 	 * --------------------------------------------------------------------------
